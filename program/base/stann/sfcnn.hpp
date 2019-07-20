188ca38520f005728d80503b9e25a0353548a374
pmoulon <pmoulon@gmail.com>
2019-07-20 10:11:20 -0700

    Build fix. #38

--- program/base/stann/sfcnn.hpp
+++ program/base/stann/sfcnn.hpp
@@ -24,6 +24,7 @@
 #include <algorithm>
 #include "tinycthread.h"
 #include <limits>
+#include <stdexcept>
 using namespace std;
 
 #include "pair_iter.hpp"
@@ -32,17 +33,17 @@ using namespace std;
 #include "bsearch.hpp"
 /*!
 	\mainpage STANN Doxygen Index Page
-	
-	This Doxygen API is intended for users interested in modifying 
+
+	This Doxygen API is intended for users interested in modifying
 	the STANN library.  For instructions on using STANN, see the
 	STANN webpage at http://www.compgeom.com/~stann or the README.txt
 	file located in the main directory of the STANN distribution.
 
-	The API is still being updated.  For requests/comments/errors, 
+	The API is still being updated.  For requests/comments/errors,
 	please contact us at stann @ compgeom dot com
 */
 
-/*! 
+/*!
   \file
   \brief Space filling curve nearest neighbor search
   This file contains the implementation of a space filling curve
@@ -51,18 +52,18 @@ using namespace std;
 
 using namespace std;
 
-/*! 
-  \brief A space filling curve nearest neighbor class.  
-  
+/*!
+  \brief A space filling curve nearest neighbor class.
+
   This is the workhorse class for the sfcnn class.
   The Space Filling Curve Nearest Neighbor (SFCNN) algorithm sorts the
   input data set into 2-order Morton ordering. Nearest neighbors are then
-  calculated based on that curve. The algorithm has a runtime of   O(ln(N)), 
+  calculated based on that curve. The algorithm has a runtime of   O(ln(N)),
   a construction time of O(Nlog(N)), and a space requirement of
   O(N). The query functions of the algorithm are thread-safe.
 */
 template <typename Point>
-class sfcnn_work 
+class sfcnn_work
 {
   template<typename P, unsigned int D, typename N> friend class sfcnn;
 public:
@@ -70,7 +71,7 @@ public:
     \brief Constructor
   */
   sfcnn_work();
-  
+
   /*!
     \brief Destructor
   */
@@ -81,7 +82,7 @@ private:
     \brief Nearest Neighbor search function
     Searches for the k nearest neighbors to the point q.  The answer
     vector returned will contain the indexes to the answer points
-    This function is thread-safe.  
+    This function is thread-safe.
     \param q The query point
     \param k The number of neighbors to return
     \param nn_idx Answer vector
@@ -106,11 +107,11 @@ private:
   void compute_bounding_box(Point q, Point &q1, Point &q2, double r);
   void sfcnn_work_init(int num_threads);
 
-  void ksearch_common(Point q, unsigned int k, long unsigned int j, qknn &que, float Eps);	
-  
-  inline void recurse(int s, int n, Point q, 
+  void ksearch_common(Point q, unsigned int k, long unsigned int j, qknn &que, float Eps);
+
+  inline void recurse(int s, int n, Point q,
 		      qknn &ans, Point &q1, Point &q2, int bb, int bt);
-	
+
 };
 
 template<typename Point>
@@ -160,11 +161,11 @@ void sfcnn_work<Point>::recurse(int s,     // Starting index
   if(n < 4)
     {
       if(n == 0) return;
-		
+
       bool update=false;
       for(int i=0;i < n;++i)
 	{
-	  if((s+i >= initial_scan_lower_range) 
+	  if((s+i >= initial_scan_lower_range)
 	     && (s+i < initial_scan_upper_range))
 	    continue;
 	  update = ans.update(points[s+i].sqr_dist(q), pointers[s+i]) || update;
@@ -173,53 +174,53 @@ void sfcnn_work<Point>::recurse(int s,     // Starting index
 	compute_bounding_box(q, bound_box_lower_corner, bound_box_upper_corner, sqrt(ans.topdist()));
       return;
     }
-  
+
   if((s+n/2 >= initial_scan_lower_range) && (s+n/2 < initial_scan_upper_range))
     {
     }
   else if(ans.update(points[s+n/2].sqr_dist(q), pointers[s+n/2]))
     compute_bounding_box(q, bound_box_lower_corner, bound_box_upper_corner, sqrt(ans.topdist()));
-  
+
   double dsqb = lt.dist_sq_to_quad_box(q,points[s], points[s+n-1]);
-  
+
   //cout << "dsqb: " << dsqb << endl;
   //cout << "dist: " << ans.topdist() << endl;
   //cout << "p1  : " << points[s] << endl;
   //cout << "p2  : " << points[s+n-1] << endl;
   if(dsqb > ans.topdist())
     return;
-  
-	
+
+
   if(lt(q,points[s+n/2]))
     {
       //search_queue.push(pair<int, int>(s, n/2));
-      recurse(s, n/2, q, ans, 
-	      bound_box_lower_corner, 
-	      bound_box_upper_corner, 
-	      initial_scan_lower_range, 
+      recurse(s, n/2, q, ans,
+	      bound_box_lower_corner,
+	      bound_box_upper_corner,
+	      initial_scan_lower_range,
 	      initial_scan_upper_range);
       if(lt(points[s+n/2],bound_box_upper_corner))
 	//search_queue.push(pair<int, int>(s+n/2+1, n-n/2-1));
-	recurse(s+n/2+1,n-n/2-1, q, ans, 
-		bound_box_lower_corner, 
-		bound_box_upper_corner, 
-		initial_scan_lower_range, 
+	recurse(s+n/2+1,n-n/2-1, q, ans,
+		bound_box_lower_corner,
+		bound_box_upper_corner,
+		initial_scan_lower_range,
 		initial_scan_upper_range);
     }
   else
     {
-      recurse(s+n/2+1, n-n/2-1, q, ans, 
-	      bound_box_lower_corner, 
-	      bound_box_upper_corner, 
-	      initial_scan_lower_range, 
+      recurse(s+n/2+1, n-n/2-1, q, ans,
+	      bound_box_lower_corner,
+	      bound_box_upper_corner,
+	      initial_scan_lower_range,
 	      initial_scan_upper_range);
       //search_queue.push(pair<int, int>(s+n/2+1, n-n/2-1));
       if(lt(bound_box_lower_corner,points[s+n/2]))
 	//search_queue.push(pair<int, int>(s, n/2));
-	recurse(s, n/2, q, ans, 
-		bound_box_lower_corner, 
-		bound_box_upper_corner, 
-		initial_scan_lower_range, 
+	recurse(s, n/2, q, ans,
+		bound_box_lower_corner,
+		bound_box_upper_corner,
+		initial_scan_lower_range,
 		initial_scan_upper_range);
     }
 }
@@ -244,16 +245,16 @@ void sfcnn_work<Point>::ksearch_common(Point q, unsigned int k, long unsigned in
 {
   Point bound_box_lower_corner, bound_box_upper_corner;
   Point low, high;
-  
+
   que.set_size(k);
   eps=1.0+Eps;
   if(query_point_index >= (k)) query_point_index -= (k);
   else query_point_index=0;
-  
+
   int initial_scan_upper_range=query_point_index+2*k+1;
   if(initial_scan_upper_range > (int)points.size())
     initial_scan_upper_range = points.size();
-  
+
   low = points[query_point_index];
   high = points[initial_scan_upper_range-1];
   for(int i=query_point_index;i<initial_scan_upper_range;++i)
@@ -261,7 +262,7 @@ void sfcnn_work<Point>::ksearch_common(Point q, unsigned int k, long unsigned in
       que.update(points[i].sqr_dist(q), pointers[i]);
     }
   compute_bounding_box(q, bound_box_lower_corner, bound_box_upper_corner, sqrt(que.topdist()));
-  
+
   if(lt(bound_box_upper_corner, high) && lt(low,bound_box_lower_corner))
     {
       //cout << "Inital search break!" << endl;
@@ -269,10 +270,10 @@ void sfcnn_work<Point>::ksearch_common(Point q, unsigned int k, long unsigned in
       //cout << "Bb2: " << bound_box_upper_corner << endl;
       return;
     }
-  
+
   //Recurse through the entire set
-  recurse(0, points.size(), q, que, 
-	  bound_box_lower_corner, 
+  recurse(0, points.size(), q, que,
+	  bound_box_lower_corner,
 	  bound_box_upper_corner,
 	  query_point_index,
 	  initial_scan_upper_range);
@@ -281,10 +282,10 @@ void sfcnn_work<Point>::ksearch_common(Point q, unsigned int k, long unsigned in
 }
 
 template<typename Point>
-void sfcnn_work<Point>::ksearch(Point q, unsigned int k, 
+void sfcnn_work<Point>::ksearch(Point q, unsigned int k,
 			      vector<long unsigned int> &nn_idx, float Eps)
 {
-  long unsigned int query_point_index;  
+  long unsigned int query_point_index;
   qknn que;
   query_point_index = BinarySearch(points, q, lt);
   ksearch_common(q, k, query_point_index, que, Eps);
@@ -292,10 +293,10 @@ void sfcnn_work<Point>::ksearch(Point q, unsigned int k,
 }
 
 template<typename Point>
-void sfcnn_work<Point>::ksearch(Point q, unsigned int k, 
+void sfcnn_work<Point>::ksearch(Point q, unsigned int k,
 				vector<long unsigned int> &nn_idx, vector<double> &dist, float Eps)
 {
-  long unsigned int query_point_index;  
+  long unsigned int query_point_index;
   qknn que;
   query_point_index = BinarySearch(points, q, lt);
   ksearch_common(q, k, query_point_index, que, Eps);
@@ -304,7 +305,7 @@ void sfcnn_work<Point>::ksearch(Point q, unsigned int k,
 
 /*!
   \brief Nearest Neighbor search class
-  
+
   This class is a wrapper class for the sfcnn_work class.
   There are several template specializations for this class
   that select the appropriate seperated float type to be used
@@ -317,15 +318,15 @@ template <typename Point, unsigned int Dim, typename NumType>
 class sfcnn
 {
 public:
-  /*! 
+  /*!
     \brief Default Constructor
   */
   sfcnn(){};
-  /*! 
+  /*!
     \brief Constructor
-    
+
     Constructs a nearest neighbor data structure using the
-    given data points.  
+    given data points.
     \param *points Pointer to the first data point
     \param N number of data points
     \param num_threads Currently unused.  (multiple processor construction soon!)
@@ -338,10 +339,10 @@ public:
 
   /*!
     \brief Nearest Neighbor search function
-    
+
     Searches for the k nearest neighbors to the point q.  The answer
     vector returned will contain the indexes to the answer points
-    This function is thread-safe.  
+    This function is thread-safe.
     \param q The query point
     \param k The number of neighbors to return
     \param nn_idx Answer vector
@@ -353,11 +354,11 @@ public:
   };
   /*!
     \brief Nearest Neighbor search function
-    
+
     Searches for the k nearest neighbors to the point q.  The answer
     vector returned will contain the indexes to the answer points.
     The distance vector contains the square distances to the answer points.
-    This function is thread-safe.  
+    This function is thread-safe.
     \param q The query point
     \param k The number of neighbors to return
     \param nn_idx Answer vector
@@ -374,7 +375,7 @@ private:
   {
     NN.points.resize(N);
     NN.pointers.resize(N);
-    
+
     for(long int i=0;i < N;++i)
       {
 	NN.pointers[i] = (i);
@@ -417,7 +418,7 @@ private:
   {
     NN.points.resize(N);
     NN.pointers.resize(N);
-    
+
     for(long int i=0;i < N;++i)
       {
 	NN.pointers[i] = i;
@@ -460,7 +461,7 @@ private:
   {
     NN.points.resize(N);
     NN.pointers.resize(N);
-    
+
     for(long int i=0;i < N;++i)
       {
 	NN.pointers[i] = i;
@@ -501,7 +502,7 @@ private:
   {
     NN.points.resize(N);
     NN.pointers.resize(N);
-    
+
     for(long int i=0;i < N;++i)
       {
 	NN.pointers[i] = i;
